9a989fa96e529f7ae10d50c963716ddc130b17dc
Roberto Della Pasqua <76778531+RDP1974@users.noreply.github.com>
2025-07-08 00:24:22 +0200

    Add files via upload

--- WM1.pas
+++ WM1.pas
@@ -3,12 +3,28 @@ unit WM1;
 interface
 
 uses
-  System.SysUtils, System.Classes, Web.HTTPApp, FireDAC.Phys.MySQLDef,
-  FireDAC.Stan.Intf, FireDAC.Phys, FireDAC.Phys.MySQL, FireDAC.Stan.Option,
-  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
-  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.ConsoleUI.Wait,
-  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, Data.DB,
-  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
+  System.SysUtils,
+  System.Classes,
+  Web.HTTPApp,
+  FireDAC.Phys.MySQLDef,
+  FireDAC.Stan.Intf,
+  FireDAC.Phys,
+  FireDAC.Phys.MySQL,
+  FireDAC.Stan.Option,
+  FireDAC.Stan.Error,
+  FireDAC.UI.Intf,
+  FireDAC.Phys.Intf,
+  FireDAC.Stan.Def,
+  FireDAC.Stan.Pool,
+  FireDAC.Stan.Async,
+  FireDAC.ConsoleUI.Wait,
+  FireDAC.Stan.Param,
+  FireDAC.DatS,
+  FireDAC.DApt.Intf,
+  FireDAC.DApt,
+  FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client,
+  Data.DB;
 
 type
   TWM = class(TWebModule)
